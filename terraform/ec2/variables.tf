cbefc35021f8e22975f500eb25007afb124eb06b
natan <your@email.here>
2025-09-18 12:56:33 +0300

    added baseline terraform provisioning for the ec2 and a deployment script

---
+++ terraform/ec2/variables.tf
@@ -0,0 +1,44 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "Existing EC2 key pair name"
+}
+
+variable "allowed_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "volume_size_gb" {
+  type    = number
+  default = 8
+}
+
+variable "project" {
+  type    = string
+  default = "my-devops-cicd-demo"
+}
+
+variable "image_repo" {
+  type    = string
+  default = "s1natex/my-devops-cicd-demo"
+}
+
+variable "api_tag" {
+  type    = string
+  default = "api-latest"
+}
+
+variable "web_tag" {
+  type    = string
+  default = "web-latest"
+}
