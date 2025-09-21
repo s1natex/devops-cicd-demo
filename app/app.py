614836b9d5184b2f31a447a708aa12a144e73ecc
natan <your@email.here>
2025-09-21 11:31:46 +0300

    built initial basic flask app

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    return "Hello, World!"
+
+
+@app.get("/healthz")
+def healthz():
+    return jsonify(status="ok"), 200
+
+
+if __name__ == "__main__":
+    app.run(port=8000)
