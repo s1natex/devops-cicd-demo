025227cd1076e16e98db295ba61e2b00a0dc3f20
natan <your@email.here>
2025-09-28 22:18:11 +0300

    test sync

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return "Hello, World!! From ArgoCD sync!"
+    return "Hello, World!! From ArgoCD sync!, Now it auto updates tags!"
 
 @app.get("/healthz")
 def healthz():
